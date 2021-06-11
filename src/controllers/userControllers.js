e2ffa1acb04ec81160ec607587a3e1eb0fd4d93e
kasterra <kasterra@naver.com>
2021-06-11 04:38:23 +0000

    set up login form

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,21 +1,64 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) =>
     res.render("joinAccount", { pageTitle: "Create Account" });
 export const postJoin = async (req, res) => {
     console.log(req.body);
-    const { name, username, email, password, location } = req.body;
-    await User.create({
-        name,
-        username,
-        email,
-        password,
-        location,
+    const { name, username, email, password, password2, location } = req.body;
+    if (password !== password2) {
+        return res.status(400).render("joinAccount", {
+            pageTitle: "Create Account",
+            errorMessage: "Password Confirmation doesn't match",
+        });
+    }
+    const existsDuplicated = await User.exists({
+        $or: [{ username }, { email }],
     });
-    return res.redirect("/login");
+    if (existsDuplicated) {
+        return res.status(400).render("joinAccount", {
+            pageTitle: "Create Account",
+            errorMessage: "This Username/Email is Already Taken",
+        });
+    }
+    try {
+        await User.create({
+            name,
+            username,
+            email,
+            password,
+            location,
+        });
+        return res.redirect("/login");
+    } catch (error) {
+        return res.status(400).render("joinAccount", {
+            pageTitle: "Create Account",
+            errorMessage: error._message,
+        });
+    }
 };
 export const edit = (req, res) => res.send("edit user");
 export const remove = (req, res) => res.send("remove user");
-export const login = (req, res) => res.send("login");
+export const getLogin = (req, res) =>
+    res.render("login", { pageTitle: "Login" });
+export const postLogin = async (req, res) => {
+    const { username, password } = req.body;
+    const pageTitle = "Login";
+    const user = await User.findOne({ username });
+    if (!userExists) {
+        return res.status(400).render("login", {
+            pageTitle,
+            errorMessage: "An account with this username doesn't exists",
+        });
+    }
+    const doesPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!doesPasswordMatch) {
+        return res.status(400).render("login", {
+            pageTitle,
+            errorMessage: "Wrong password",
+        });
+    }
+    return res.redirect("/");
+};
 export const logout = (req, res) => res.send("logout");
 export const see = (req, res) => res.send("see");
--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -9,7 +9,7 @@ export const watch = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
     if (!video) {
-        return res.render("404", { pageTitle: "VIDEO NOT FOUND" });
+        return res.status(404).render("404", { pageTitle: "VIDEO NOT FOUND" });
     }
     return res.render("watch", { pageTitle: video.title, video });
 };
@@ -24,7 +24,7 @@ export const postEdit = async (req, res) => {
     const { title, description, hashtags } = req.body;
     const video = await Video.exists({ _id: id });
     if (!video) {
-        return res.render("404", { pageTitle: "Video Not Found" });
+        return res.status(404).render("404", { pageTitle: "Video Not Found" });
     }
     await Video.findByIdAndUpdate(id, {
         title,
@@ -52,7 +52,7 @@ export const postUpload = async (req, res) => {
     try {
         await video.save();
     } catch (error) {
-        return res.render("upload", {
+        return res.status(400).render("upload", {
             pageTitle: "Upload Video",
             errorMessage: error._message,
         });
@@ -72,7 +72,7 @@ export const search = async (req, res) => {
     if (keyword) {
         videos = await Video.find({
             title: {
-              $regex: new RegExp(keyword,"i")
+                $regex: new RegExp(keyword, "i"),
             },
         });
     }
