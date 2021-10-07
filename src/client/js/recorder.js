cd667bb98a3be6d940628ac057a2d8230833d528
kasterra <kasterra@naver.com>
2021-10-07 22:06:35 +0900

    recording video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,28 +1,49 @@
-import { createFFmpeg, fetchFile }  from "@ffmpeg/ffmpeg";
-
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleDownload = async() =>{
-    const ffmpeg = createFFmpeg({log:true});
-    await ffmpeg.load();
+let stream;
+let recorder;
+let videoFile;
 
-    ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFIle));
-
-    const a = document.createElement("a");
-    a.href = videoFile;
-    a.download = "Myrecording.webm";
-    document.body.appendChild(a);
-    a.click();
-}
-
-const handleStart = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({
-        audio: true,
-        video: true,
-    });
-    video.srcObject = stream;
-    video.play();
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "MyRecording.webm";
+  document.body.appendChild(a);
+  a.click();
 };
+
+const handleStop = () => {
+  startBtn.innerText = "Download!";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleDownload);
+  recorder.stop();
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
+  recorder.ondataavailable = (event) => {
+    videoFile = URL.createObjectURL(event.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true,
+  });
+  video.srcObject = stream;
+  video.play();
+};
+
+init();
 
 startBtn.addEventListener("click", handleStart);
