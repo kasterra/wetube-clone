ad67e47e36126f4abac5e679181256f7f137c29d
kasterra <kasterra@naver.com>
2021-10-08 11:11:04 +0900

    crossOrigin Issue in modern browser

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,34 +1,39 @@
 import multer from "multer";
 
 export const localsMiddleware = (req, res, next) => {
-    res.locals.loggedIn = Boolean(req.session.loggedIn);
-    res.locals.siteName = "Wetube";
-    res.locals.loggedInUser = req.session.user || "";
-    next();
+  res.locals.loggedIn = Boolean(req.session.loggedIn);
+  res.locals.siteName = "Wetube";
+  res.locals.loggedInUser = req.session.user || "";
+  next();
 };
 
 export const protectorMiddleware = (req, res, next) => {
-    if (!req.session.loggedIn) {
-        return res.redirect("/login");
-    }
-    next();
+  if (!req.session.loggedIn) {
+    return res.redirect("/login");
+  }
+  next();
 };
 
 export const publicOnlyMiddleware = (req, res, next) => {
-    if (req.session.loggedIn) {
-        return res.redirect("/");
-    }
-    next();
+  if (req.session.loggedIn) {
+    return res.redirect("/");
+  }
+  next();
 };
 export const avatarUpload = multer({
-    dest: "uploads/avatars/",
-    limits: {
-        fileSize: 3000000,
-    },
+  dest: "uploads/avatars/",
+  limits: {
+    fileSize: 3000000,
+  },
 });
 export const videoUpload = multer({
-    dest: "uploads/videos/",
-    limits: {
-        fileSize: 10000000,
-    },
+  dest: "uploads/videos/",
+  limits: {
+    fileSize: 10000000,
+  },
 });
+export const crossOrigin = (req, res, next) => {
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,29 +1,29 @@
 import express from "express";
 import {
-    getEdit,
-    deleteVideo,
-    watch,
-    postEdit,
-    getUpload,
-    postUpload,
+  getEdit,
+  deleteVideo,
+  watch,
+  postEdit,
+  getUpload,
+  postUpload,
 } from "../controllers/videoControllers";
-import { protectorMiddleware, videoUpload } from "../middlewares";
+import { crossOrigin, protectorMiddleware, videoUpload } from "../middlewares";
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter
-    .route("/:id([0-9a-f]{24})/edit")
-    .all(protectorMiddleware)
-    .get(getEdit)
-    .post(postEdit);
+  .route("/:id([0-9a-f]{24})/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(postEdit);
 videoRouter
-    .route("/:id([0-9a-f]{24})/delete")
-    .all(protectorMiddleware)
-    .get(deleteVideo);
+  .route("/:id([0-9a-f]{24})/delete")
+  .all(protectorMiddleware)
+  .get(deleteVideo);
 videoRouter
-    .route("/upload")
-    .all(protectorMiddleware)
-    .get(getUpload)
-    .post(videoUpload.single("video"), postUpload);
+  .route("/upload")
+  .all(protectorMiddleware, crossOrigin)
+  .get(getUpload)
+  .post(videoUpload.single("video"), postUpload);
 
 export default videoRouter;
