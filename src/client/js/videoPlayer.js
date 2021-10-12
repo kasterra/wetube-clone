203c460a4e6eb263853fca040d738713f9a3d029
kasterra <kasterra@naver.com>
2021-10-12 17:53:52 +0900

    video comment

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -14,106 +14,107 @@ let controlsTimeout = null;
 let controlsMovementTimeout = null;
 
 const init = () => {
-    video.volume = volumeValue;
+  video.volume = volumeValue;
 };
 
 const handlePlayClick = (e) => {
-    //if the video is playing, pause it
-    if (video.paused) {
-        video.play();
-    } else {
-        video.pause();
-    }
-    playBtn.classList = video.paused ? "fas fa-play" : "fas fa-pause";
+  //if the video is playing, pause it
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  playBtn.classList = video.paused ? "fas fa-play" : "fas fa-pause";
 };
 
 const handleMute = (e) => {
-    if (video.muted) {
-        video.muted = false;
-    } else {
-        video.muted = true;
-    }
-    muteBtn.classList = video.muted ? "fas fa-volume-up" : "fas fa-volume-mute";
-    volumeRange.value = video.muted ? 0 : volumeValue;
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.classList = video.muted ? "fas fa-volume-up" : "fas fa-volume-mute";
+  volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
 const handleVolumeChange = (event) => {
-    const {
-        target: { value },
-    } = event;
-    if (video.muted) {
-        video.muted = false;
-        muteBtn.innerText = "Mute";
-    }
-    volumeValue = value;
-    video.volume = value;
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value;
+  video.volume = value;
 };
 
 const formatTime = (seconds) =>
-    new Date(seconds * 1000).toISOString().substr(11, 8);
+  new Date(seconds * 1000).toISOString().substr(11, 8);
 
 const handleLoadedMetadata = () => {
-    totalTime.innerText = formatTime(Math.floor(video.duration));
-    timeline.max = video.duration;
+  totalTime.innerText = formatTime(Math.floor(video.duration));
+  timeline.max = video.duration;
 };
 const handleTimeUpdate = () => {
-    currentTime.innerText = formatTime(Math.floor(video.currentTime));
-    timeline.value = Math.floor(video.currentTime);
+  currentTime.innerText = formatTime(Math.floor(video.currentTime));
+  timeline.value = Math.floor(video.currentTime);
 };
 
 const handleTimelineChange = (event) => {
-    const {
-        target: { value },
-    } = event;
-    video.currentTime = value;
+  const {
+    target: { value },
+  } = event;
+  video.currentTime = value;
 };
 const handleFullscreen = (e) => {
-    const fullscreen = document.fullscreenElement;
-    if (fullscreen) {
-        fullscreenBtn.classList = "fas fa-expand";
-        document.exitFullscreen();
-    } else {
-        videoContainer.requestFullscreen();
-        fullscreenBtn.classList = "fas fa-compress";
-    }
+  const fullscreen = document.fullscreenElement;
+  if (fullscreen) {
+    fullscreenBtn.classList = "fas fa-expand";
+    document.exitFullscreen();
+  } else {
+    videoContainer.requestFullscreen();
+    fullscreenBtn.classList = "fas fa-compress";
+  }
 };
 
 const hideControls = () => {
-    videoControls.classList.remove("showing");
+  videoControls.classList.remove("showing");
 };
 
 const handleMouseMove = () => {
-    if (controlsTimeout) {
-        clearTimeout(controlsTimeout);
-        controlsTimeout = null;
-    }
-    if (controlsMovementTimeout) {
-        clearTimeout(controlsMovementTimeout);
-        controlsMovementTimeout = null;
-    }
-    videoControls.classList.add("showing");
-    controlsMovementTimeout = setTimeout(hideControls, 3000);
+  if (controlsTimeout) {
+    clearTimeout(controlsTimeout);
+    controlsTimeout = null;
+  }
+  if (controlsMovementTimeout) {
+    clearTimeout(controlsMovementTimeout);
+    controlsMovementTimeout = null;
+  }
+  videoControls.classList.add("showing");
+  controlsMovementTimeout = setTimeout(hideControls, 3000);
 };
 const handleMouseLeave = () => {
-    controlsTimeout = setTimeout(hideControls, 3000);
+  controlsTimeout = setTimeout(hideControls, 3000);
 };
 const handleKeyDown = (e) => {
-    switch (e.code) {
-        case "Space":
-            handlePlayClick();
-            break;
-        case "KeyM":
-            handleMute();
-            break;
-    }
+  if (e.target.id === "textarea") return;
+  switch (e.code) {
+    case "Space":
+      handlePlayClick();
+      break;
+    case "KeyM":
+      handleMute();
+      break;
+  }
 };
 const handleEnded = (e) => {
-    const { videoid } = videoContainer.dataset;
-    fetch(`/api/videos/${videoid}/view`, { method: "POST" });
+  const { videoid } = videoContainer.dataset;
+  fetch(`/api/videos/${videoid}/view`, { method: "POST" });
 };
 
 if (video.readyState == 4) {
-    handleLoadedMetadata();
+  handleLoadedMetadata();
 }
 
 init();
--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,5 +1,6 @@
 import Video from "../models/Video";
 import User from "../models/User";
+import Comment from "../models/Comment";
 import { isValidObjectId } from "mongoose";
 
 export const home = async (req, res) => {
@@ -11,7 +12,7 @@ export const home = async (req, res) => {
 
 export const watch = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id).populate("owner");
+  const video = await Video.findById(id).populate("owner").populate("comments");
   if (!video) {
     return res.status(404).render("404", { pageTitle: "VIDEO NOT FOUND" });
   }
@@ -126,4 +127,20 @@ export const registerView = async (req, res) => {
   video.meta.views = video.meta.views + 1;
   await video.save();
   return res.sendStatus(200);
+};
+
+export const createComment = async (req, res) => {
+  const {
+    session: { user },
+    body: { text },
+    params: { id },
+  } = req;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.sendStatus(404);
+  }
+  const comment = await Comment.create({ text, owner: user._id, video: id });
+  video.comments.push(comment._id);
+  video.save();
+  return res.sendStatus(201);
 };
